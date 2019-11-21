6690269df3bc8cc4e3e95689b930b60688ad5b15
Vishvanath Surwashe <vsurwshe@gmail.com>
2019-11-21 16:08:33 +0530

    0.0.2: Starting Auth Server Implemations

---
+++ src/main/java/com/vishva/microservices/Config/ResourceServerConfig.java
@@ -0,0 +1,34 @@
+package com.vishva.microservices.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@Configuration
+class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers("/login", "/oauth/authorize").and().authorizeRequests().anyRequest()
+                .authenticated().and().formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(authenticationManagerBean()).inMemoryAuthentication().withUser("vishva")
+                .password("123456").roles("ADMIN");
+    }
+
+}
---
+++ src/main/java/com/vishva/microservices/Resource/HelloResource.java
@@ -0,0 +1,18 @@
+package com.vishva.microservices.Config.Resource;
+
+import java.security.Principal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+
+}
